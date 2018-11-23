b873f94adccc8c93692db199dc1e41c310fb774b
mohandesnorouzi <mc_norouzi@yahoo.com>
2018-11-23 09:37:05 +0330

    finish footer responsive

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  innerWidth: any;
+
+  constructor() {
+  }
+
+  @HostListener('window:resize', ['$event'])
+  BiggerThanTablet() {
+    this.innerWidth = window.innerWidth;
+    return this.innerWidth > 768;
+  }
+
+  Tablet() {
+    this.innerWidth = window.innerWidth;
+    return (this.innerWidth <= 768 && this.innerWidth > 576);
+  }
+
+  Mobile() {
+    this.innerWidth = window.innerWidth;
+    console.log(this.innerWidth);
+    return this.innerWidth <= 576;
+  }
+
 
   ngOnInit() {
   }
